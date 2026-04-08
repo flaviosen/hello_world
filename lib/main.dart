4dd73cc2aac1bd55992fa6b5a37cc05b0476970d
flaviosen <flavio.izidoro@aluno.senai.br>
2026-04-08 11:29:22 -0300

    situacao de aprendizagem: 100% concluída

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Container(
+            width: 330,
+            height: 220, // aumentei um pouco pra caber o nome completo
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.deepPurple,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.black54,
+                  blurRadius: 15,
+                  offset: Offset(0, 8),
+                )
+              ],
+            ),
+
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const Text("bem vindo", style: TextStyle(fontSize: 24),),
               const SizedBox(width: 20),
