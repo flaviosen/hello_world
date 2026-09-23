6d1a1b4bb14e1bf3dd74de7e957401c6ad673c19
flaviosen <flavio.izidoro@aluno.senai.br>
2026-09-23 15:51:59 -0300

    atividade banco interno

--- lib/Banco Interno/database_helper.dart
+++ lib/Banco Interno/database_helper.dart
@@ -1,7 +1,7 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-import 'Banco Interno/tarefa.dart';
+import 'tarefa.dart';
 
 class DatabaseHelper {
   static Database? _database;
